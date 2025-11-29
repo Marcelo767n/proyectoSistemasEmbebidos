74469444031a02c858a08410436b589ac6151a9f
AndresAyllon <roberto.ayllon@ucb.edu.bo>
2025-11-29 01:08:41 -0500

    modularizacion codigo arduino IDE

--- Codigos/control_pelota_plataformaMOD/funciones.h
+++ Codigos/control_pelota_plataformaMOD/funciones.h
@@ -1,18 +1,32 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
+
 #include <Arduino.h>
-Servo servoX;
-Servo servoY;
+#include <ESP32Servo.h>
 
 // Pines PWM del ESP32 
 #define PIN_X  18
 #define PIN_Y  19
+
+// Serial
 #define BAUDRATE 115200
+
+// Rangos de ángulos
 #define ANG_INI 90
 #define ANG_MIN 60
 #define ANG_MAX 120
-String inputString = "";         
-boolean stringComplete = false;  
 
+// Variables globales (solo declaración)
+extern String inputString;
+extern bool stringComplete;
+
+// Declaración de servos
+extern Servo servoX;
+extern Servo servoY;
+
+// Prototipos
 void iniciar();
+void funcionprincipal();
+void serialEvent();
+
 #endif
